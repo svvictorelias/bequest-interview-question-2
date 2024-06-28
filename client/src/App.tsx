89309f6b6993504e70476216ae768398ad61a0cb
Victor Elias <svvictorelias@gmail.com>
2024-06-27 22:32:15 -0300

    feat: make challenge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,68 @@
 import React, { useEffect, useState } from "react";
+import CryptoJS from "crypto-js";
+import Logo from "./assets/bequest_logo.jpeg";
 
 const API_URL = "http://localhost:8080";
 
+const buttonStyle = {
+  fontSize: "1.2rem",
+  padding: "10px 20px",
+  backgroundColor: "#007bff",
+  color: "white",
+  border: "none",
+  borderRadius: "5px",
+  cursor: "pointer",
+  transition: "background-color 0.3s ease"
+};
+
+const inputStyle = {
+  fontSize: "1.5rem",
+  padding: "10px",
+  width: "300px",
+  maxWidth: "80%"
+};
+
+const messageStyle = {
+  marginTop: "20px",
+  fontSize: "1.2rem",
+  fontStyle: "italic",
+  color: "#28a745"
+};
+
+const errorMessageStyle = {
+  ...messageStyle,
+  color: "#dc3545"
+};
+
+const containerStyle: React.CSSProperties = {
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "20px",
+  fontSize: "1.5rem",
+  overflow: "hidden"
+};
+
+const linkContainerStyle: React.CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  gap: "20px"
+};
+
+const logoStyle: React.CSSProperties = {
+  position: "absolute",
+  top: "20px",
+  width: "200px"
+};
+
 function App() {
-  const [data, setData] = useState<string>();
+  const [data, setData] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [messageStyleOverride, setMessageStyleOverride] = useState<
+    React.CSSProperties | undefined
+  >(undefined);
 
   useEffect(() => {
     getData();
@@ -21,47 +80,100 @@ function App() {
       body: JSON.stringify({ data }),
       headers: {
         Accept: "application/json",
-        "Content-Type": "application/json",
-      },
+        "Content-Type": "application/json"
+      }
     });
 
     await getData();
   };
 
   const verifyData = async () => {
-    throw new Error("Not implemented");
+    const hmac = generateHMAC(data);
+    const response = await fetch(`${API_URL}/verify`, {
+      method: "POST",
+      body: JSON.stringify({ data, hmac }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    });
+    const result = await response.json();
+    setMessage(result.message);
+    if (result.status === "error") {
+      setMessageStyleOverride(errorMessageStyle);
+    } else {
+      setMessageStyleOverride(undefined);
+    }
+  };
+
+  const recoverData = async () => {
+    const response = await fetch(`${API_URL}/recover`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    });
+    const result = await response.json();
+    setMessage(result.message);
+    setData(result.data.data);
+  };
+
+  const generateHMAC = (data: string) => {
+    const secret = "B3qu6st@V1ct0R";
+    return CryptoJS.HmacSHA256(data, secret).toString(CryptoJS.enc.Hex);
   };
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        position: "absolute",
-        padding: 0,
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        gap: "20px",
-        fontSize: "30px",
-      }}
-    >
-      <div>Saved Data</div>
+    <div style={containerStyle}>
+      <img src={Logo} alt="Bequest Logo" style={logoStyle} />
+      <div
+        style={{ fontSize: "2rem", fontWeight: "bold", textAlign: "center" }}
+      >
+        Saved Data
+      </div>
       <input
-        style={{ fontSize: "30px" }}
+        style={inputStyle}
         type="text"
         value={data}
-        onChange={(e) => setData(e.target.value)}
+        onChange={e => setData(e.target.value)}
       />
 
-      <div style={{ display: "flex", gap: "10px" }}>
-        <button style={{ fontSize: "20px" }} onClick={updateData}>
+      <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+        <button style={buttonStyle} onClick={updateData}>
           Update Data
         </button>
-        <button style={{ fontSize: "20px" }} onClick={verifyData}>
+        <button style={buttonStyle} onClick={verifyData}>
           Verify Data
         </button>
+        <button style={buttonStyle} onClick={recoverData}>
+          Recover Data
+        </button>
+      </div>
+      <div style={messageStyleOverride ?? messageStyle}>{message}</div>
+
+      <div>
+        <div style={linkContainerStyle}>
+          <a
+            href="https://www.linkedin.com/in/svvictorelias/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ fontSize: "1.5rem", color: "#0077b5" }}
+          >
+            LinkedIn
+          </a>
+          <p>
+            {" <- "} Victor Elias {" -> "}
+          </p>
+          <a
+            href="https://github.com/svvictorelias"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ fontSize: "1.5rem", color: "#333" }}
+          >
+            GitHub
+          </a>
+        </div>
       </div>
     </div>
   );
--- server/app.ts
+++ server/app.ts
@@ -1,22 +1,61 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
+import crypto from "crypto";
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+const secret = "B3qu6st@V1ct0R";
+
+interface Database {
+  data: string;
+  hmac: string;
+}
+
+let database: Database = { data: "Hello World", hmac: "" };
+let backup: Database = { ...database };
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
+const generateHMAC = (data: string): string => {
+  return crypto.createHmac("sha256", secret).update(data).digest("hex");
+};
 
-app.get("/", (req, res) => {
+const verifyHMAC = (data: string, hmac: string): boolean => {
+  const computedHMAC = generateHMAC(data);
+  return computedHMAC === hmac;
+};
+
+database.hmac = generateHMAC(database.data);
+
+app.get("/", (req: Request, res: Response) => {
   res.json(database);
 });
 
-app.post("/", (req, res) => {
-  database.data = req.body.data;
+app.post("/", (req: Request, res: Response) => {
+  const newData: string = req.body.data;
+  const newHMAC: string = generateHMAC(newData);
+  database = { data: newData, hmac: newHMAC };
+  backup = { ...database };
   res.sendStatus(200);
+});
+
+app.post("/verify", (req: Request, res: Response) => {
+  const { data, hmac } = req.body;
+  if (verifyHMAC(data, hmac)) {
+    res.json({ status: "success", message: "Data is valid and untampered" });
+  } else {
+    res.json({ status: "error", message: "Data has been tampered with" });
+  }
+});
+
+app.post("/recover", (req: Request, res: Response) => {
+  database = { ...backup };
+  res.json({
+    status: "success",
+    message: "Data has been restored",
+    data: database
+  });
 });
 
 app.listen(PORT, () => {
